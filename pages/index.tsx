88d67ebdcd984ccbf01c170b5ff76cb334d40c42
Nabarun Gogoi <nabarun@deepstacksoft.com>
2025-06-17 16:53:47 +0530

    For deleting main deployment

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://www.laconic.com/">Laconic!</a>
         </h1>
-        <p>Testing with laconic mainnet</p>
+        <p>Testing with laconic mainnet to delete main deployment</p>
 
         <div className={styles.grid}>
 
